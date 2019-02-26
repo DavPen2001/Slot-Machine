2e4c5c42dcd07369b649ed3702339470c77a2e1e
DavPen2001 <david_rodriguez@davincischools.org>
2019-02-26 14:13:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,36 +4,36 @@ c = document.getElementById("slot3");
 mySound = document.getElementById('you-win');
 
 function nextImage(el){
-if (el.src.match("images/cherry_slot.PNG")){
-el.src = "images/lemon_slot.PNG";
-} else if (el.src.match("images/lemon_slot.PNG")){
-el.src = "images/orange_slot.PNG";
-} else if (el.src.match("images/orange_slot.PNG")){
-el.src = "images/cherry_slot.PNG";
+if (el.src.match("cherry_slot.PNG")){
+el.src = "lemon_slot.PNG";
+} else if (el.src.match("lemon_slot.PNG")){
+el.src = "orange_slot.PNG";
+} else if (el.src.match("orange_slot.PNG")){
+el.src = "cherry_slot.PNG";
 } else {
 }
 }
 
 function nextImage2(el){
-if (el.src.match("images/lemon_slot.PNG")){
-el.src = "images/bar_slot.PNG";
-} else if (el.src.match("images/bar_slot.PNG")){
-el.src = "images/cherry_slot.PNG";
-} else if (el.src.match("images/cherry_slot.PNG")){
-el.src = "images/lemon_slot.PNG";
+if (el.src.match("lemon_slot.PNG")){
+el.src = "bar_slot.PNG";
+} else if (el.src.match("bar_slot.PNG")){
+el.src = "cherry_slot.PNG";
+} else if (el.src.match("cherry_slot.PNG")){
+el.src = "lemon_slot.PNG";
 } else {
 }
 }
 
 function nextImage3(el){
-if (el.src.match("images/orange_slot.PNG")){
-el.src = "images/cherry_slot.PNG";
-} else if (el.src.match("images/cherry_slot.PNG")){
-el.src = "images/seven_slot.PNG";
-} else if (el.src.match("images/seven_slot.PNG")){
-el.src = "images/watermelon_slot.PNG";
-}else if (el.src.match("images/watermelon_slot.PNG")){
-el.src = "images/orange_slot.PNG";
+if (el.src.match("orange_slot.PNG")){
+el.src = "cherry_slot.PNG";
+} else if (el.src.match("cherry_slot.PNG")){
+el.src = "seven_slot.PNG";
+} else if (el.src.match("seven_slot.PNG")){
+el.src = "watermelon_slot.PNG";
+}else if (el.src.match("watermelon_slot.PNG")){
+el.src = "orange_slot.PNG";
 } else {
 }
 }
@@ -41,7 +41,7 @@ el.src = "images/orange_slot.PNG";
 
 
 function checkColors(el){
-if (a.src.match("images/cherry_slot.PNG") && b.src.match("images/cherry_slot.PNG") && c.src.match("images/cherry_slot.PNG")){
+if (a.src.match("cherry_slot.PNG") && b.src.match("cherry_slot.PNG") && c.src.match("cherry_slot.PNG")){
 mySound.play();
 a.classList.add("opaque");
 b.classList.add("opaque");
